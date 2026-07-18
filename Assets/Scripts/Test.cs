e2d4393ad049a6eb8bfb7f2635ce540becb410c8
Yimyeam <yimyeam_r@silpakorn.edu>
2026-07-18 10:13:23 +0700

    Test

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private int n = 0;
+    private float timer = 0f;
+    void Awake()
+    {
+        Debug.Log("Awake"); 
+    }
+
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        n++;
+
+        Debug.Log(Time.deltaTime);
+
+        if (timer >= 1f)
+        {
+            Debug.Log(n);
+            timer = 0f;
+            n = 0;
+        }
+    }
+}
