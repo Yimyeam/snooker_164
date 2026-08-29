ff0d4a64d2ef38f78b85e75da82422942214199f
oYoiii <yimyeam_r@silpakorn.edu>
2026-08-29 18:57:17 +0700

    Loading fix

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -17,7 +17,7 @@ public class MainMenu : MonoBehaviour
 
     public void StartNewGame()
     {
-        SceneManager.LoadScene("Scene01");
+        SceneManager.LoadScene("Loading");
     }
 
     public void ExitGame()
